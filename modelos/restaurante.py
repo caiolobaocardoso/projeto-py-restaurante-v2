c0e868596880acd812dc69ff6c774e45b473023d
Caio Cardoso <caiolobaocardoso@gmail.com>
2024-03-11 11:42:15 -0300

    class method

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -10,17 +10,21 @@ class Restaurante:
     def __str__(self):
         return f'{self._nome} | {self._categoria}'
     
-    def listar_restaurantes():
-        
-        for restaurante in Restaurante.restaurantes:
+    @classmethod
+    def listar_restaurantes(cls):
+        for restaurante in cls.restaurantes:
             print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
 
     @property
     def ativo(self):
         return 'Ativado' if self._ativo else 'Desativado'
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
 
 restaurante_praca = Restaurante('praca', 'Gourmet')
+restaurante_praca.alternar_estado()
 restaurante_pizza = Restaurante('pizza', 'Italiano')
 
 
